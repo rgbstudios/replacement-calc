04a42aff765f7cc0f3d9bc63a6137b47b17bda4b
RGB Studios <justingolden21@gmail.com>
2018-10-01 18:00:57 -0700

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+<img src="icon/icon.svg" width="64px">
+
+# Picking Without Replacement Probability Calculator - RGB Studios
+
+### Use Replacement Calc to calculate the probabilities of picking a certain number of objects without replacement, such as picking marbles or cards
+
+<a href="https://rgbstudios.org/binomial-calc">rgbstudios.org/replacement-calc</a>
+
+
+<tr>
+<td><p>Total number of items</p></td>
+<td><b>N</b></td>
+<td><input autofocus id="NInput" class="form-control" type="number" value="60" step="1" min="1" max="9999"><br></td>
+</tr>
+
+<tr>
+<td><p>Number of distinct items</p></td>
+<td><b>m</b></td>
+<td><input id="mInput" class="form-control" type="number" value="22" min="0" max="9999"><br></td>
+</tr>
+
+<tr>
+<td><p>Number of items picked</p></td>
+<td><b>n</b></td>
+<td><input id="nInput" class="form-control" type="number" value="16" min="0" max="9999"><br></td>
+</tr>
+
+<tr>
+<td><p>Number of distinct items picked</p></td>
+<td><b>k</b></td>
+<td><input id="kInput" class="form-control" type="number" value="10" min="0" max="9999"><br></td>
+</tr>
+
+
+- Enter the probability of an event (p), the number of trials (n), and the number of successes (x) 
+
+- Click "Calculate" and view the probability of exactly that many successes, as well as the cumulative probability of less or more successes
+
+- View each of these probabilities represented in a pie chart, and view the odds of each number of successes on a bar graph, colored based on your number of successes
+
+
+<hr>
+
+V.1.0.0
+
+by Justin Golden for RGB Studios
+
+<a href="https://justingolden21.github.io">justingolden21.github.io</a>
+
+<a href="mailto:justingolden@rgbstudios.org">justingolden@rgbstudios.org</a>
+
+<img src="rgb-icon.png" width="32px"> <a href="https://rgbstudios.org">rgbstudios.org</a>
+
+<a href="https://facebook.com/rgbstudios.org">facebook.com/rgbstudios.org</a>
---
+++ scripts.js
@@ -0,0 +1,273 @@
+
+/*
+v.1.0.2 5-21
+todo:
+
+fix overflow, roudning, efficiency problems for very large input
+make table more mobile friendly
+use realfavicongenerator.net
+
+*/
+
+function nchoosek(n,k){
+  let result = 1;
+  for(let i = 1; i <= k; i++){
+    result *= (n+1-i)/i;
+  }
+  return result;
+}
+
+function equal(N, m, n, k) {
+  return nchoosek(m,k) * nchoosek(N-m,n-k) / nchoosek(N,n);
+}
+function less(N, m, n, k) {
+  let result = 0;
+  for(let i=0; i<k; i++) {
+    result += equal(N,m,n,i);
+  }
+  return result;
+}
+function greater(N, m, n, k) {
+  let result = 0;
+  for(let i=n; i>k; i--) {
+    result += equal(N,m,n,i);
+  }
+  return result;
+}
+
+// function mean(p,n,x) {
+//   return n*p;
+// }
+// function variance(p,n,x) {
+//   return n * p * (1-p);
+// }
+// function stddev(p,n,x) {
+//   return Math.sqrt(variance(p,n,x));
+// }
+
+function round(num, digits) {
+  return (Math.round(num*Math.pow(10,digits) ) / Math.pow(10,digits) );
+}
+
+document.onkeyup = function(e) {
+  let code = e.keyCode ? e.keyCode : e.which;
+  if(code == 13){ //enter
+    calc();
+  }
+  // else if(code == 73) { //"i"
+  //   document.getElementById('infoModalButton').click();
+  // }
+  // else if(code == 78) { //"n"
+  //   document.getElementById('nightButton').click(); 
+  // }
+}
+
+function clear() {
+  document.getElementById("NInput").value = '';
+  document.getElementById("mInput").value = '';
+  document.getElementById("nInput").value = '';
+  document.getElementById("kInput").value = '';
+  console.log('Cleared');
+}
+
+//https://stackoverflow.com/questions/3900701/onclick-go-full-screen?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+// function toggleFullscreen() {
+//   if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+//    (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+//     if (document.documentElement.requestFullScreen) {  
+//       document.documentElement.requestFullScreen();  
+//     } else if (document.documentElement.mozRequestFullScreen) {  
+//       document.documentElement.mozRequestFullScreen();  
+//     } else if (document.documentElement.webkitRequestFullScreen) {  
+//       document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+//     }  
+//   } else {  
+//     if (document.cancelFullScreen) {  
+//       document.cancelFullScreen();  
+//     } else if (document.mozCancelFullScreen) {  
+//       document.mozCancelFullScreen();  
+//     } else if (document.webkitCancelFullScreen) {  
+//       document.webkitCancelFullScreen();  
+//     }  
+//   }  
+// }
+
+function calc() {
+
+  let N = parseInt(document.getElementById("NInput").value);
+  let m = parseInt(document.getElementById("mInput").value);
+  let n = parseInt(document.getElementById("nInput").value);
+  let k = parseInt(document.getElementById("kInput").value);
+
+
+  console.log("Calculating... Inputs: " + N + ", " + m + ", " + n + ", " + k);
+
+  let errorP = document.getElementById("errorP");
+  let errorAlert = document.getElementById("errorAlert");
+
+  errorP.innerHTML = "";
+  errorAlert.style.display = "none";
+
+  if(isNaN(N) || isNaN(m) || isNaN(n) || isNaN(k) ) {
+    errorP.innerHTML = "N, m, n, and k must be numbers";
+    errorAlert.style.display = "block";
+    return;
+  }
+  if(N <= 0 || m <= 0 || n <= 0 || k <= 0) {
+    errorP.innerHTML = "n and x cannot be less than 0";
+    errorAlert.style.display = "block";
+    return;
+  }
+  if(N >= 1000 || m >= 1000 || n >= 1000 || k >= 1000) {
+    errorP.innerHTML = "n and x must be less than 1000";
+    errorAlert.style.display = "block";
+    return;
+  }
+  if(N < m) {
+    errorP.innerHTML = "N must be &ge; m";
+    errorAlert.style.display = "block";
+    return;
+  }
+  if(n < k) {
+    errorP.innerHTML = "n must be &ge; k";
+    errorAlert.style.display = "block";
+    return;
+  }
+
+  //TODO: check for whole numbers
+
+  //only if valid inputs
+  history.replaceState({}, "", "?N=" + N + "&m=" + m + "&n=" + n + "&k=" + k);
+
+  //display results
+
+  // document.getElementById("chooseOutput").value = Math.round(nchoosek(n, x));
+  document.getElementById("equalOutput").value = round(equal(N, m, n, k),10);
+  document.getElementById("lessOutput").value = round(less(N, m, n, k),10);
+  document.getElementById("greaterOutput").value = round(greater(N, m, n, k),10);
+  document.getElementById("lessEqualOutput").value = round(equal(N, m, n, k) + less(N, m, n, k),10);
+  document.getElementById("greaterEqualOutput").value = round(equal(N, m, n, k) + greater(N, m, n, k),10);
+  // document.getElementById("infoP").innerHTML = "Mean (&mu;) = " + round(mean(p, n, x),10) + "<br>Variance (&sigma;) = " + round(variance(p, n, x),10) + "<br>  Standard Deviation (&sigma;<sup>2</sup>) = " + round(stddev(p, n, x),10);
+
+  //google charts
+  // google.charts.load('current', {'packages':['corechart','bar']});
+  // google.charts.setOnLoadCallback(drawChart);
+
+  // function drawChart() {
+  //   //pie
+  //   let data = google.visualization.arrayToDataTable([
+  //     ['Set', 'Odds'],
+  //     ['P(X=x)', round(equal(p, n, x),5)],
+  //     ['P(X<x)', round(less(p, n, x),5)],
+  //     ['P(X>x)', round(greater(p, n, x),5)]
+  //   ]);
+
+  //   let options = {
+  //     'title':'Binomial Distribution', 
+  //     legend: {textStyle:{color: night ? '#ccc' : '#333'}}, 
+  //     titleTextStyle:{color: night ? '#ccc' : '#333'}, 
+  //     'width':'75%', 
+  //     colors: ['#339', '#933', '#393'], 
+  //     backgroundColor: { fill:'transparent' } 
+  //   };
+
+  //   let chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  //   chart.draw(data, options);
+
+  //   //bar
+  //   let chartdata = [['','', { role: 'style' } ]];
+  //   for(let numSuccesses = 0; numSuccesses<n+1; numSuccesses++) {
+  //     let color = "#933";
+  //     if(numSuccesses==x) {color="#339";}
+  //     if(numSuccesses>x) {color="#393";}
+  //     chartdata.push([numSuccesses, equal(p,n,numSuccesses), color]);
+  //   }
+
+  //   data = google.visualization.arrayToDataTable(chartdata);
+
+  //   options = {
+  //     title: 'Odds by number of successes', 
+  //     titleTextStyle:{color: night ? '#ccc' : '#333'},
+  //     legend: 'none',
+  //     chartArea: {width: '75%', legend:{position: 'none'} },
+  //     hAxis: {
+  //       title: 'Number of Successes x',
+  //       textStyle:{color: night ? '#ccc' : '#333'},
+  //       titleTextStyle:{color: night ? '#ccc' : '#333'}
+  //     },
+  //     vAxis: {
+  //       title: 'P(x)',
+  //       textStyle:{color: night ? '#ccc' : '#333'},
+  //       titleTextStyle:{color: night ? '#ccc' : '#333'}
+  //     },
+  //     backgroundColor: { fill:'transparent' }
+  //   };
+  
+  //   chart = new google.visualization.ColumnChart(document.getElementById('barchart'));
+  //   chart.draw(data, options);
+  // } //end drawChart
+
+} //end calc
+
+// let night = false;
+
+
+$(document).ready(function() {
+
+  let url = new URL(window.location.href);
+  let N = url.searchParams.get("N");
+  let m = url.searchParams.get("m");
+  let n = url.searchParams.get("n");
+  let k = url.searchParams.get("k");
+  console.log("loaded: " + N + ", " + m + ", " + n + ", " + k);
+  document.getElementById("NInput").value = N || "60"; //TODO: edit default values
+  document.getElementById("mInput").value = m || "22";
+  document.getElementById("nInput").value = n || "16";
+  document.getElementById("kInput").value = k || "10";
+
+  console.log(document.getElementById("NInput").value);
+  console.log(document.getElementById("mInput").value);
+  console.log(document.getElementById("nInput").value);
+  console.log(document.getElementById("kInput").value);
+
+  document.getElementById("NInput").select();
+
+  calc();
+
+  //copy buttons
+  $('button.copy').click(function() {
+    let input = $(this).prev('input'); //select adjacent input
+    console.log(input);
+    input.prop('disabled',false); //.attr and .disabled didn't work...
+    input.select();
+    document.execCommand('copy');
+    input.prop('disabled',true);
+  });
+
+  $('#clearButton').click(function() { //setting onclick in html didn't work for some reason...
+    clear();
+  });
+
+  // $('#fullscreenButton').click(function() {
+  //   toggleFullscreen();
+  // });
+
+  // $('#nightButton').click(function() {
+  //   night = !night;
+  //   if(night) {
+  //     $('#nightStyles').prop('href','styles-night.css');
+  //     $('#metaColor1').prop('content','#333');
+  //     $('#metaColor2').prop('content','#333');
+  //     $('#metaColor3').prop('content','#333');
+  //     calc(); //update google chart with correct color. when your night mode function is O(n^3)...
+  //   }
+  //   else {
+  //     $('#nightStyles').prop('href','');
+  //     $('#metaColor1').prop('content','#ccc');
+  //     $('#metaColor2').prop('content','#ccc');
+  //     $('#metaColor3').prop('content','#ccc');
+  //     calc();
+  //   }
+  // });
+
+});
